97d79bd286d36a4fe7aee49a2ffff0c4510793af
Anna W <aweinmacher@gmail.com>
2018-02-03 20:50:02 +0200

    remove expense

--- src/App.js
+++ src/App.js
@@ -6,27 +6,28 @@ import ExpenseDisplay from './expense-display';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { expenses: [] };
     this.addExpense = this.addExpense.bind(this);
     this.removeExpense = this.removeExpense.bind(this);
+    this.state = { expenses: [] };
   }
   addExpense(expense) {
     this.setState({ expenses: this.state.expenses.concat(expense) }, function () {
       console.log(this.state.expenses)
     });
   }
-  removeExpense(expense) {
-    var temp = this.state.expenses.slice();
-    temp.splice(expense.myKey, 1);
-    this.setState({ expenses: temp }, function () {
-      console.log(this.state.expenses)
-    });
+  removeExpense(expenseIndex) {
+    this.setState(
+      (prevState) => {
+        return {expenses: prevState.expenses.filter((item, i) => { return (i !== expenseIndex) })}
+      }
+    );
   }
+
   render() {
     return (
       <div className="App">
-        <ExpenseForm addExpense={this.addExpense} />
-        <ExpenseDisplay expenses={this.state.expenses} />
+        <ExpenseForm addExpense={this.addExpense} btnText="Add Expense" />
+        <ExpenseDisplay expenses={this.state.expenses} removeExpense={this.removeExpense} />
       </div>
     );
   }
--- src/expense-form.js
+++ src/expense-form.js
@@ -4,16 +4,16 @@ import SingleInput from './single-input';
 class ExpenseForm extends Component {
     constructor(props) {
         super(props);
-        this.state = { amt: 0, cat: "", descr: "" };
+        this.state = { amt: 0, cat: "" , descr: "" };
         this.changeExpense = this.changeExpense.bind(this);
         this.onBtnClick = this.onBtnClick.bind(this);
         console.log(this.state);
     }
     changeExpense(e) {
         this.setState({ [e.target.id]: e.target.value });
-        // why we're not using callback here?
+        // how to write with callback?
     }
-    onBtnClick(){
+    onBtnClick() {
         this.props.addExpense(this.state);
         this.setState({ amt: 0, cat: '', descr: '' }, () => {
             console.log(this.state);
@@ -23,10 +23,10 @@ class ExpenseForm extends Component {
         return (
             <div>
                 <form>
-                    <SingleInput id="amt" type="number" placeholder="$" content={this.state.amt} controlFunc={this.changeExpense}/>
-                    <SingleInput id="cat" type="text" placeholder="Category" content={this.state.cat} controlFunc={this.changeExpense}/>
-                    <SingleInput id="descr" type="text" placeholder="Description" content={this.state.descr} controlFunc={this.changeExpense}/>
-                    <button type="button" onClick={this.onBtnClick}>Add Expense</button>
+                    <SingleInput id="amt" type="number" placeholder="$" content={this.state.amt} controlFunc={this.changeExpense} />
+                    <SingleInput id="cat" type="text" placeholder="Category" content={this.state.cat} controlFunc={this.changeExpense} />
+                    <SingleInput id="descr" type="text" placeholder="Description" content={this.state.descr} controlFunc={this.changeExpense} />
+                    <button type="button" onClick={this.onBtnClick}>{this.props.btnText}</button>
                 </form>
             </div>
         );
